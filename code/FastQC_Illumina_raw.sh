602c57861ebb2c2c88229b90f07e469a3dd47d76
Marina Kyriakou <marinky@pelle1.uppmax.uu.se>
2026-04-14 11:42:04 +0200

    FastQC raw Illumina code

---
+++ code/FastQC_Illumina_raw.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+ 
+#SBATCH -A uppmax2026-1-61 
+#SBATCH -p pelle 
+#SBATCH -c 1 
+#SBATCH -t 00:15:00 
+#SBATCH -J FastQC-Illumina-raw 
+#SBATCH --mail-type=ALL 
+
+#SBATCH --output=FastQC-Illumina-raw.out
+ 
+# Load modules 
+module load FastQC
+ 
+# Your commands 
+fastqc /home/marinky/Genome_Analysis/1_Zhang_2017/genomics_data/Illumina/*\ --outdir /home/marinky/Genome-Analysis-Project-VT2026/results/Genome_Assembly/Illumina-Nanopore/QC-Illumina_Nanopore/FastQC-Illumina-raw 
