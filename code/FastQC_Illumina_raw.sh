32201b6b0fa46d0493ce8f6fd60c1727fce91e56
Marina Kyriakou <marinky@pelle1.uppmax.uu.se>
2026-04-15 08:47:07 +0200

    Fixed some errors

--- code/FastQC_Illumina_raw.sh
+++ code/FastQC_Illumina_raw.sh
@@ -10,8 +10,7 @@
 #SBATCH --output=FastQC-Illumina-raw.out
  
 # Load modules 
-module load FastQC
+module load FastQC/0.12.1-Java-17
  
 # Your commands 
-fastqc /home/marinky/Genome_Analysis/1_Zhang_2017/genomics_data/Illumina/*\ 
-	--outdir /home/marinky/Genome-Analysis-Project-VT2026/results/Genome_Assembly/Illumina-Nanopore/QC-Illumina_Nanopore 
+fastqc /proj/uppmax2026-1-61/Genome_Analysis/1_Zhang_2017/genomics_data/Illumina/* --outdir /home/marinky/Genome-Analysis-Project-VT2026/results/Genome_Assembly/Illumina-Nanopore/QC-Illumina_Nanopore 
