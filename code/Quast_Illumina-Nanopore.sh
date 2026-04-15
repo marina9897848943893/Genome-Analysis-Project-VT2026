a3cf22847452d76e5511ca5064cc9e5c6d449847
Marina Kyriakou <marinky@pelle2.uppmax.uu.se>
2026-04-15 16:03:32 +0200

    Assembly evaluation of Illumina and Nanopore using Quast

---
+++ code/Quast_Illumina-Nanopore.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+ 
+#SBATCH -A uppmax2026-1-61 
+#SBATCH -p pelle 
+#SBATCH -c 1
+#SBATCH -t 00:30:00 
+#SBATCH -J Quast_Illumina-Nanopore
+#SBATCH --mail-type=ALL 
+
+#SBATCH --output=Quast_Illumina-Nanopore.out
+
+# Load modules 
+module load QUAST/5.3.0-gfbf-2024a
+
+# Quast assembly evaluation on PacBio reads
+python /sw/arch/eb/software/QUAST/5.3.0-gfbf-2024a/bin/quast.py /home/marinky/Genome-Analysis-Project-VT2026/results/Genome_Assembly/Illumina-Nanopore/SPAdes-assembly-Illumina_Nanopore/contigs.fasta \
+	-o /home/marinky/Genome-Analysis-Project-VT2026/results/Genome_Assembly//Illumina-Nanopore/Quast-evaluation-Illumina_Nanopore
