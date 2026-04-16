6b76c0f166d0a5f03d071f7744a1a45447567ccf
Marina Kyriakou <marinky@pelle2.uppmax.uu.se>
2026-04-16 15:40:56 +0200

    fix conflict try

--- code/Quast_Illumina-Nanopore.sh
+++ code/Quast_Illumina-Nanopore.sh
@@ -12,6 +12,10 @@
 # Load modules 
 module load QUAST/5.3.0-gfbf-2024a
 
+<<<<<<< Updated upstream
 # Quast assembly evaluation on PacBio reads
+=======
+# Quast assembly evaluation on Illumina and Nanopore reads
+>>>>>>> Stashed changes
 python /sw/arch/eb/software/QUAST/5.3.0-gfbf-2024a/bin/quast.py /home/marinky/Genome-Analysis-Project-VT2026/results/Genome_Assembly/Illumina-Nanopore/SPAdes-assembly-Illumina_Nanopore/contigs.fasta \
 	-o /home/marinky/Genome-Analysis-Project-VT2026/results/Genome_Assembly//Illumina-Nanopore/Quast-evaluation-Illumina_Nanopore
