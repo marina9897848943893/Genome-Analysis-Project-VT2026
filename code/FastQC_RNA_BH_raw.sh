223e53dfb6ed62383717641999837fe66311b3fb
Marina Kyriakou <marinky@pelle2.uppmax.uu.se>
2026-05-10 18:10:58 +0200

    code for quality control on rna seq

---
+++ code/FastQC_RNA_BH_raw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+ 
+#SBATCH -A uppmax2026-1-61 
+#SBATCH -p pelle 
+#SBATCH -c 1 
+#SBATCH -t 00:15:00 
+#SBATCH -J FastQC-RNA-BH-raw 
+#SBATCH --mail-type=ALL 
+
+#SBATCH --output=FastQC-RNA-BH-raw.out
+ 
+# Load modules 
+module load FastQC/0.12.1-Java-17
+ 
+# FastQC on Illumina reads 
+fastqc /home/marinky/Genome_Analysis/1_Zhang_2017/transcriptomics_data/RNA-Seq_BH/raw/* \
+	--outdir /home/marinky/Genome-Analysis-Project-VT2026/results/RNA-seq/QC_BH_raw
