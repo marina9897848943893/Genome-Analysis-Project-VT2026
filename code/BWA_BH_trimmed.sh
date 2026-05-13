e73cb65a40e5953810f77bf25aae126ca82b8626
Marina Kyriakou <marinky@pelle2.uppmax.uu.se>
2026-05-13 16:38:28 +0200

    changed files

--- code/BWA_BH_trimmed.sh
+++ code/BWA_BH_trimmed.sh
@@ -16,34 +16,35 @@ module load SAMtools/1.22.1-GCC-13.3.0
 # Paths
 INPUT_DIR=/home/marinky/Genome_Analysis/1_Zhang_2017/transcriptomics_data/RNA-Seq_BH/trimmed
 
-OUTDIR=/home/marinky/Genome-Analysis-Project-VT2026/results/RNA-seq/BAM-BH
+OUTDIR=/proj/uppmax2026-1-61/nobackup/Marina2/SAM_files
 
 REF_GENOME=/home/marinky/Genome-Analysis-Project-VT2026/results/Genome_Assembly/PacBio/Canu-assembly-PacBio/E.faeciumE745.contigs.fasta
 
 # Index reference genome
-bwa index $REF_GENOME
+bwa index "$REF_GENOME"
 
 # Loop for all paired sequences
 for SAMPLE in ERR1797972 ERR1797973 ERR1797974
 do
-    READ1=${INPUT_DIR}/trim_paired_${SAMPLE}_pass_1.fastq.gz
-    READ2=${INPUT_DIR}/trim_paired_${SAMPLE}_pass_2.fastq.gz
+    READ1="${INPUT_DIR}/trim_paired_${SAMPLE}_pass_1.fastq.gz"
+    READ2="${INPUT_DIR}/trim_paired_${SAMPLE}_pass_2.fastq.gz"
 
-    OUT_SAM=${OUTDIR}/${SAMPLE}.sam
-    OUT_BAM=${OUTDIR}/${SAMPLE}.sorted.bam
+    OUT_SAM="${OUTDIR}/${SAMPLE}.sam"
+    OUT_BAM="${OUTDIR}/${SAMPLE}.bam"
+    #OUT_SORT_BAM="${OUTDIR}/${SAMPLE}.sorted.bam"
 
     # BWA on BH trimmed paired sequences
-    bwa mem -M $REF_GENOME $READ1 $READ2 > $OUT_SAM
+    bwa mem -M "$REF_GENOME" "$READ1" "$READ2" > "$OUT_SAM"
 
     # Convert SAM to BAM and then sort
-    samtools view -b $OUT_SAM | \
-    samtools sort -o $OUT_BAM
-    
+    samtools view -bS "$OUT_SAM" -o "$OUT_BAM"
+    #samtools sort "$OUT_BAM" -o "$OUT_SORT_BAM"    
+
     # Index BAM
-    samtools index $OUT_BAM
+    #samtools index "$OUT_SORT_BAM"
 
     # Remove SAM
-    rm $OUT_SAM
+    #rm "$OUT_SAM"
 
 done
 
--- code/BWA_Serum_trimmed.sh
+++ code/BWA_Serum_trimmed.sh
@@ -16,33 +16,34 @@ module load SAMtools/1.22.1-GCC-13.3.0
 # Paths
 INPUT_DIR=/home/marinky/Genome_Analysis/1_Zhang_2017/transcriptomics_data/RNA-Seq_Serum/trimmed
 
-OUTDIR=/home/marinky/Genome-Analysis-Project-VT2026/results/RNA-seq/BAM-Serum
+OUTDIR=/proj/uppmax2026-1-61/nobackup/Marina2/SAM_files
 
 REF_GENOME=/home/marinky/Genome-Analysis-Project-VT2026/results/Genome_Assembly/PacBio/Canu-assembly-PacBio/E.faeciumE745.contigs.fasta
 
 # Index reference genome
-bwa index $REF_GENOME
+bwa index "$REF_GENOME"
 
 # Loop for all paired sequences
 for SAMPLE in ERR1797969 ERR1797970 ERR1797971
 do
-    READ1=${INPUT_DIR}/trim_paired_${SAMPLE}_pass_1.fastq.gz
-    READ2=${INPUT_DIR}/trim_paired_${SAMPLE}_pass_2.fastq.gz
+    READ1="${INPUT_DIR}/trim_paired_${SAMPLE}_pass_1.fastq.gz"
+    READ2="${INPUT_DIR}/trim_paired_${SAMPLE}_pass_2.fastq.gz"
 
-    OUT_SAM=${OUTDIR}/${SAMPLE}.sam
-    OUT_BAM=${OUTDIR}/${SAMPLE}.sorted.bam
+    OUT_SAM="${OUTDIR}/${SAMPLE}.sam"
+    OUT_BAM="${OUTDIR}/${SAMPLE}.bam"
+    #OUT_SORT_BAM="${OUTDIR}/${SAMPLE}.sorted.bam"
 
     # BWA on Serum trimmed paired sequences
-    bwa mem -M $REF_GENOME $READ1 $READ2 > $OUT_SAM
+    bwa mem -M "$REF_GENOME" "$READ1" "$READ2" > "$OUT_SAM"
 
     # Convert SAM to BAM and then sort
-    samtools view -b $OUT_SAM | \
-    samtools sort -o $OUT_BAM
+    samtools view -bS "$OUT_SAM" -o "$OUT_BAM"
+    #samtools sort "$OUT_BAM" -o "$OUT_SORT_BAM"
 
     # Index BAM
-    samtools index $OUT_BAM
+    #samtools index "$OUT_SORT_BAM"
 
     # Remove SAM
-    rm $OUT_SAM
+    #rm "$OUT_SAM"
 
 done
