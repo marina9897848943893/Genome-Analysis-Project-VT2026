04a61b0634dc5779d9bda54cabce57ba901c384c
Marina Kyriakou <marinky@pelle2.uppmax.uu.se>
2026-05-21 11:50:07 +0200

    qc on trimmed seq

---
+++ code/FastQC_RNA_BH_Serum_preprocessed.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 1
+#SBATCH -t 00:15:00
+#SBATCH -J FastQC-RNA-BH-Seru-preprocessed
+#SBATCH --mail-type=ALL
+
+#SBATCH --output=FastQC-RNA-BH-Seru-preprocessed.out
+
+# Load modules
+module load FastQC/0.12.1-Java-17
+
+# FastQC on trimmed BH reads
+TRIM_DIR=/home/marinky/Genome-Analysis-Project-VT2026/results/RNA-seq/Trim_BH
+OUT_BH=/home/marinky/Genome-Analysis-Project-VT2026/results/RNA-seq/FastQC_Trim_BH
+OUT_SERUM=/home/marinky/Genome-Analysis-Project-VT2026/results/RNA-seq/FastQC_Trim_Serum
+
+for SAMPLE in ERR1797972 ERR1797973 ERR1797974
+do
+    fastqc \
+        ${TRIM_DIR}/${SAMPLE}_1.trim.paired.fq.gz \
+        ${TRIM_DIR}/${SAMPLE}_2.trim.paired.fq.gz \
+        --outdir "$OUT_BH"
+done
+
+# FastQC on trimmed Serum reads
+for SAMPLE in ERR1797969 ERR1797970 ERR1797971
+do
+    fastqc \
+        ${TRIM_DIR}/${SAMPLE}_1.trim.paired.fq.gz \
+        ${TRIM_DIR}/${SAMPLE}_2.trim.paired.fq.gz \
+        --outdir "$OUT_SERUM"
+done
+
